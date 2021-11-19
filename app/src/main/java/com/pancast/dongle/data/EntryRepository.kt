4f59cf4a460b18ce6a30b6bfd15a0dca8aea16b5
Aastha Mehta <aasthakm@mpi-sws.org>
2021-11-19 04:05:06 -0800

    fix compilation error

--- app/src/main/java/com/pancast/dongle/data/EntryRepository.kt
+++ app/src/main/java/com/pancast/dongle/data/EntryRepository.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 
 class EntryRepository(private val entryDao: EntryDao) {
 
-    val entries: LiveData<List<Entry>> = entryDao.getAll(0)
+    var entries: LiveData<List<Entry>> = entryDao.getAll(0)
 
     fun addEntry(entry: Entry) {
         entryDao.insert(entry)
