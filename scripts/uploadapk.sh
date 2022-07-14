5e04e8705d320a442074c486aa3e99558e265251
Aastha Mehta <aasthakm@gmail.com>
2022-07-13 17:58:12 -0700

    update script to upload apk debug and release versions to server

--- scripts/uploadapk.sh
+++ scripts/uploadapk.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-rootdir="/Users/aasthakm/workspace-research/pancast/dev2/pancast-android"
-apkdir="$rootdir/app/build/outputs/apk/debug"
-apkfile="$apkdir/app-debug.apk"
+version=$1
+
+if [[ $# -lt 1 ]]; then
+  echo "Usage: ./scripts/uploadapk.sh <versionnumber>"
+  echo "E.g.: ./scripts/uploadapk.sh 2.0"
+  exit -1
+fi
 
 servid="pancast"
 server="pancast.cs.ubc.ca"
-servdir="/home/$servid/downloads"
-outfile="pancast-dongle-2.0.apk"
+#servdir="/home/$servid/downloads"
+servdir="/var/www/html/$server/apk"
 
-cmd="scp -rq $apkfile $servid@$server:$servdir/$outfile"
-echo "$cmd"
-eval "$cmd"
+rootdir="/Users/aasthakm/workspace-research/pancast/dev2/pancast-android"
+#apkdir="$rootdir/app/build/outputs/apk/debug"
 
-cmd="ssh $servid@$server \"chmod u+x,g+wx $servdir/$outfile\""
-echo "$cmd"
-eval "$cmd"
+for cfg in "debug" "release"; do
+  apkdir="$rootdir/app/$cfg"
+  apkfile="$apkdir/app-$cfg.apk"
+  outfile="pancast-dongle-$cfg-2.0.apk"
+
+  cmd="scp -rq $apkfile $servid@$server:$servdir/$outfile"
+  echo "$cmd"
+  eval "$cmd"
+
+  cmd="ssh $servid@$server \"chmod u+x,g+wx $servdir/$outfile\""
+  echo "$cmd"
+  eval "$cmd"
+done
